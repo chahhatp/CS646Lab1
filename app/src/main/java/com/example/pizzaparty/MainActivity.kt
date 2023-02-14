377abbee55996e5b96cc3bf3034f5bd7a76f1f2b
chahhatp <chahhatp@gmail.com>
2023-02-13 23:50:25 -0800

    Lab1 commit

--- app/src/main/java/com/example/pizzaparty/MainActivity.kt
+++ app/src/main/java/com/example/pizzaparty/MainActivity.kt
@@ -11,7 +11,6 @@ import kotlin.math.ceil
 const val SLICES_PER_PIZZA = 8
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var numAttendEditText: EditText
     private lateinit var numPizzasTextView: TextView
     private lateinit var howHungryRadioGroup: RadioGroup
